001c93d0cc50e99de766c65100a96c49b0fb0854
Tye Stanley <tyestanley090@gmail.com>
2023-01-13 18:46:27 -0800

    completed homepage for mobile screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,9 +12,93 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="bg-black text-white hover:bg-white">Home</h1>
-      </main>
+      <div className="h-screen w-screen overflow-hidden bg-[url('../assets/main-background.jpg')] bg-cover">
+        <div className="flex h-full w-full flex-col justify-center bg-white bg-opacity-70">
+          <section className="text-center">
+            <h2 className="text-lg font-semibold">Full-Stack Developer</h2>
+            <h1 className="mt-2 text-2xl font-bold tracking-widest">
+              TYE STANLEY
+            </h1>
+
+            <div className="mx-auto mt-2 h-1 w-[140px] bg-black" />
+
+            <div className="mx-auto mt-10 flex w-[320px] justify-around">
+              <Link
+                className="flex h-[35px] w-[90px] items-center justify-center rounded border border-black hover:animate-bounce hover:bg-blue-200"
+                href="/about"
+              >
+                <Image
+                  src={require("../assets/icons/person-icon.png")}
+                  alt="Person Icon"
+                  width={"15"}
+                  height={"15"}
+                />
+                <p className="ml-1">About</p>
+              </Link>
+              <Link
+                className="flex h-[35px] w-[90px] items-center justify-center rounded border border-black hover:animate-bounce hover:bg-blue-200"
+                href="/portfolio"
+              >
+                <Image
+                  src={require("../assets/icons/briefcase-icon.png")}
+                  alt="Linkedin Icon"
+                  width={"15"}
+                  height={"15"}
+                />
+                <p className="ml-1">Portfolio</p>
+              </Link>
+              <Link
+                className="flex h-[35px] w-[90px] items-center justify-center rounded border border-black hover:animate-bounce hover:bg-blue-200"
+                href="/contact"
+              >
+                <Image
+                  src={require("../assets/icons/chat-bubble-icon.png")}
+                  alt="Stack Overflow Icon"
+                  width={"15"}
+                  height={"15"}
+                />
+                <p className="ml-1">Contact</p>
+              </Link>
+            </div>
+
+            <div className="mx-auto mt-10 flex w-[200px] justify-between">
+              <Link
+                href="https://github.com/TyeStanley"
+                className="border-1 rounded border border-white p-1 hover:animate-bounce hover:bg-blue-200"
+              >
+                <Image
+                  src={require("../assets/icons/github-icon.png")}
+                  alt="Github Icon"
+                  width={"40"}
+                  height={"40"}
+                />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/tye-stanley/"
+                className="border-1 rounded border border-white p-1 hover:animate-bounce hover:bg-blue-200"
+              >
+                <Image
+                  src={require("../assets/icons/linkedin-icon.png")}
+                  alt="Linkedin Icon"
+                  width={"40"}
+                  height={"40"}
+                />
+              </Link>
+              <Link
+                href="https://stackoverflow.com/users/17858669/tye-stanley"
+                className="border-1 rounded border border-white p-1 hover:animate-bounce hover:bg-blue-200"
+              >
+                <Image
+                  src={require("../assets/icons/stack-overflow-icon.png")}
+                  alt="Stack Overflow Icon"
+                  width={"40"}
+                  height={"40"}
+                />
+              </Link>
+            </div>
+          </section>
+        </div>
+      </div>
     </>
   );
 }
